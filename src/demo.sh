0e7e9b26c2b0485c2e86a38d1768ccd3c98af78e
Wangxin-neuq <77545135+Wangxin-neuq@users.noreply.github.com>
2021-01-17 16:31:27 +0800

    Add files via upload

---
+++ src/demo.sh
@@ -0,0 +1,7 @@
+
+#python main.py --model PACN --save pacn_X2 --scale 2 --reset --save_results --patch_size 96
+#python main.py --model PACN --save pacn_X3 --scale 3 --reset --save_results --patch_size 144
+#python main.py --model PACN --save pacn_X4 --scale 4 --reset --save_results --patch_size 192
+#python main.py --model PACN --data_test Manga109 --scale 3 --pre_train ../experiment/model/pacn_X3.pt --test_only --save_results
+
+
